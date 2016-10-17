c80ee8bb702c1ae99fcce08c152191a53a9e9626
Alex Skorulis <skorulis@gmail.com>
2016-10-18 10:39:49 +1100

    Add corners

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ function setInitialFields() {
 	var form = $("form")[0];
 	var urlVars = getUrlVars();
 	form["brand"].value = sanitiseText(urlVars["brand"]);
+	form["corners"].checked = sanitiseBool(urlVars["corners"]);
 }
 
 function getFormObject() {
@@ -14,6 +15,13 @@ function getFormObject() {
 	var form = $("form")[0];
 	opt.format = form["format"].value;
 	opt.brand = form["brand"].value;
+	opt.corners = form["corners"].checked;
+
+	opt.pageWidth = A4Height / 2;
+	opt.pageHeight = A4Width / 2;
+
+	opt.singleWidth = opt.pageWidth / 3;
+	opt.singleHeight = opt.pageHeight / 4;
 
 	console.log(opt);
 	return opt;
@@ -23,7 +31,7 @@ function redraw() {
 	var opt = getFormObject();
 	var c1 = $("#single-label")[0];
 	var c2 = $("#full-label")[0];
-	setupPageSize(opt.format,c1,c2);
+	setupPageSize(opt,c1,c2);
 
 	var ctx = c1.getContext("2d");
 	drawSingleLabel(ctx,opt);
@@ -34,16 +42,16 @@ function redraw() {
 
 	ctx.lineWidth = 1;
 
-	for (var i = 1; i < cols; ++i) {
+	for (var i = 0; i < cols+1; ++i) {
 		ctx.beginPath();
-		ctx.moveTo(i*c1.width,0);
-		ctx.lineTo(i*c1.width,c2.height);
+		ctx.moveTo(i * c1.width,0);
+		ctx.lineTo(i * c1.width,c2.height);
 		ctx.stroke();
 	}
-	for (var i = 1; i < rows; ++i) {
+	for (var i = 0; i < rows+1; ++i) {
 		ctx.beginPath();
-		ctx.moveTo(0,i*c1.height);
-		ctx.lineTo(c2.width,i*c1.height);
+		ctx.moveTo(0,i * c1.height);
+		ctx.lineTo(c2.width,i * c1.height);
 		ctx.stroke();	
 	}
 
@@ -57,21 +65,42 @@ function redraw() {
 	
 }
 
-function setupPageSize(format,c1,c2) {
-	c2.width = A4Height/2;
-	c2.height = A4Width/2;
+function setupPageSize(opt,c1,c2) {
+	c2.width = opt.pageWidth;
+	c2.height = opt.pageHeight;
 
-	c1.width = c2.width / 3;
-	c1.height = c2.height / 4;
-
-
+	c1.width = opt.singleWidth;
+	c1.height = opt.singleHeight;
 }
 
 function drawSingleLabel(ctx,opt) {
+	var w = opt.singleWidth;
+	var h = opt.singleHeight;
 	ctx.beginPath();
 	ctx.rect(20, 20, 150, 100);
 	ctx.fillStyle = "red";
 	ctx.fill();
+
+	if(opt.corners) {
+		var cornerPct = 0.15;
+		var cornerPoints = [
+			0,h * cornerPct,
+			w * cornerPct,0,
+			w * (1 - cornerPct),0,
+			w,h * cornerPct,
+			w,h * (1 - cornerPct),
+			w * (1 - cornerPct),h,
+			w * cornerPct,h,
+			0,h * (1 - cornerPct)
+		];
+		for(var i = 0; i < 4; ++i) {
+			ctx.beginPath();
+			ctx.moveTo(cornerPoints[i * 4],cornerPoints[i * 4 + 1]);
+			ctx.lineTo(cornerPoints[i * 4 + 2],cornerPoints[i * 4 + 3]);
+			ctx.stroke();		
+		}
+	}
+
 }
 
 function sanitiseText(text) {
@@ -79,6 +108,10 @@ function sanitiseText(text) {
 		return "";
 	}
 	return text;
+}
+
+function sanitiseBool(text) {
+	return text == "on" ? true : false;
 }
 
 function getUrlVars() {
